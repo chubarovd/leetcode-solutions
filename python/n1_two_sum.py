ffcda0ad02391506aee93179fd1f0ce983a526b9
Дмитрий Чубаров <dimchu97@yandex.ru>
2021-05-21 20:50:36 +0300

    1. Two sum

---
+++ python/n1_two_sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(0, len(nums)):
+            check = dict.get(target - nums[i])
+            if check is not None:
+                return [check, i]
+            dict[nums[i]] = i
+
+
+sln = Solution()
+print(sln.twoSum([1, 1, 1, 7, 7, 2, 7, 11, 15, 2], 9))
